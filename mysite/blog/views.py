bfed299115ac84d49a39ac1fb05bd7ccf825766b
adriano-ferrari <adriano.ferrari@aluno.faculdadeimpacta.com.br>
2020-12-04 20:46:03 -0300

    Modificados os arquivos: blog/urls.py; blog/post/list.html; e blog/views.py

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
+
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
 
 
 def post_list(request):
@@ -35,5 +43,3 @@ def post_detail(request, year, month, day, post):
     return render(request,
                   'blog/post/detail.html',
                   {'post': post})
-
-# Create your views here.
