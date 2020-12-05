779c42bd72b5a39af70ae2aca80483b0ec67c97c
adriano-ferrari <adriano.ferrari@aluno.faculdadeimpacta.com.br>
2020-12-05 20:35:11 -0300

    Cap2 - Criado o arquivo mysite/blog/templates/blog/post/share.html. E alterados os arquivos: modified:   mysite/blog/templates/blog/post/detail.html

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -2,13 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
-
-
-class PostListView(ListView):
-    queryset = Post.published.all()
-    context_object_name = 'posts'
-    paginate_by = 3
-    template_name = 'blog/post/list.html'
+from .forms import EmailPostForm
+from django.core.mail import send_mail
 
 
 def post_list(request):
@@ -43,3 +38,34 @@ def post_detail(request, year, month, day, post):
     return render(request,
                   'blog/post/detail.html',
                   {'post': post})
+
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
+
+
+def post_share(request, post_id):
+    # Obtém a postagem com base no id
+    post = get_object_or_404(Post, id=post_id, status='published')
+    sent = False
+
+    if request.method == 'POST':
+        # Formulário foi submetido
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # Campos do formulário passaram pela validação
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f'{cd["name"]} recommends you read {post.title}'
+            message = f'Read {post.title} at {post_url}\n\n {cd["name"]}\'s comments: {cd["comments"]}'
+            send_mail(subject, message, 'admin@myblog.com', [cd['to']])
+            sent = True
+    else:
+        form = EmailPostForm()
+    return render(request,
+                  'blog/post/share.html',
+                  {'post': post, 'form': form,
+                   'sent': sent})
