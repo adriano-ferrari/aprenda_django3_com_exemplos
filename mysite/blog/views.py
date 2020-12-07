9a4a35fea5679be593d2f9d63d2ad019da18e187
adriano-ferrari <adriano.ferrari@aluno.faculdadeimpacta.com.br>
2020-12-07 08:53:02 -0300

    Cap2 - Criando um sistema de comentários e adicionando a funcionalidade de marcação com tags

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Post
+from .models import Post, Comment
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
-from .forms import EmailPostForm
+from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
+from taggit.models import Tag
 
 
-def post_list(request):
+def post_list(request, tag_slug=None):
     object_list = Post.published.all()
+    tag = None
+
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags_in=[tag])
+
     paginator = Paginator(object_list, 3) # três postagens por página
     page = request.GET.get('page')
     try:
@@ -16,11 +23,12 @@ def post_list(request):
         # Se a página não for um inteiro, exibe a primeira página
         posts = paginator.page(1)
     except EmptyPage:
-        # Se a página estiver fora do intervalo, exibe a útilma página de resultados
+        # Se a página estiver fora do intervalo,
+        # exibe a útilma página de resultados
         posts = paginator.page(paginator.num_pages)
     return render(request,
                   'blog/post/list.html',
-                  {'page': page, 'posts': posts})
+                  {'page': page, 'posts': posts, 'tag':tag})
 
     # posts = Post.published.all()
     # return render(request,
@@ -29,15 +37,35 @@ def post_list(request):
 
 
 def post_detail(request, year, month, day, post):
-    post = get_object_or_404(Post,
-                             slug=post,
+    post = get_object_or_404(Post, slug=post,
                              status='published',
                              publish__year=year,
                              publish__month=month,
                              publish__day=day)
+
+    # Lista dos comentários ativos para esta postagem
+    comments = post.comments.filter(active=True)
+
+    new_comment = None
+
+    if request.method == 'POST':
+        # Um comentário foi postado
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            # Cria o objeto Comment, mas não o salva no banco de dados
+            new_comment = comment_form.save(commit=False)
+            # Atribui a postagem atual ao comentário
+            new_comment.post = post
+            # Salva o comentário no banco de dados
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
     return render(request,
                   'blog/post/detail.html',
-                  {'post': post})
+                  {'post': post,
+                   'comments': comments,
+                   'new_comment': new_comment,
+                   'comment_form': comment_form})
 
 
 class PostListView(ListView):
